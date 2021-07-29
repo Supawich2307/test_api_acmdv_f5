065b3c6f884aa542e9e90c566bffab4ab8e73be5
Supakew <44920680+Supawich2307@users.noreply.github.com>
2021-07-29 17:54:57 +0700

    Update config.py

--- test_api/lib/config.py
+++ test_api/lib/config.py
@@ -1,7 +1,7 @@
 import os
 import yaml
 
-config_path = os.path.join(os.getcwd(), "test_api_acmdv_f5/test_api/lib/config.yml")
+config_path = os.path.join(os.getcwd(), "home/sysansible/test_api_acmdv_f5/test_api/lib/config.yml")
 
 f = open(config_path)
 config = yaml.safe_load(f)
